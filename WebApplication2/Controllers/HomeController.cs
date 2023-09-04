44f4f1856c9c637845ee2d9bed3f3ba5a3c94db5
Connor Arends <connor.arends@southeasttech.edu>
2023-09-03 20:24:00 -0500

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Marry";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+    }
+}
